8d7501f64b456988a8211bd0b022cb75a0ea8136
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-11-29 06:47:17 -0700

    update to use mock data files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,45 +2,14 @@ import React from 'react';
 
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
-
-/*const photos = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "2",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "3",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  }
-]*/
+import mockPhotos from './mocks/photos';
+import mockTopics from './mocks/topics';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={mockPhotos} topics={mockTopics}/>
     </div>
   );
 };
