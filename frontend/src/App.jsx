420407423076e0908876f20cedd780a4629593cb
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-12-02 08:29:48 -0700

    update hook to fetch data from api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,18 @@ import React, { useState } from 'react';
 
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
-import mockPhotos from './mocks/photos';
-import mockTopics from './mocks/topics';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const { state, updateToFavPhotoIds, onPhotoSelect, onClosePhotoDetailsModal, isPhotoInFavorites } = useApplicationData();
+  const { state, updateToFavPhotoIds, onPhotoSelect, onClosePhotoDetailsModal, isPhotoInFavorites, photoData, topicData } = useApplicationData();
 
   return (
     <div className="App">
       <HomeRoute
-        photos={mockPhotos}
-        topics={mockTopics}
+        photos={state.photoData}
+        topics={state.topicData}
         openModal={onPhotoSelect}
         markAsFavPhoto={updateToFavPhotoIds}
         isPhotoInFavorites={isPhotoInFavorites}
@@ -25,7 +23,7 @@ const App = () => {
       {state.isModalOpen && <PhotoDetailsModal
         closeModal={onClosePhotoDetailsModal}
         photo={state.selectedPhoto}
-        photos={mockPhotos}
+        photos={state.photoData}
         markAsFavPhoto={updateToFavPhotoIds}
         isPhotoInFavorites={isPhotoInFavorites}
       />}
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 
 // Define action types
 export const ACTIONS = {
@@ -6,6 +6,8 @@ export const ACTIONS = {
   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
   SET_MODAL_OPEN: 'SET_MODAL_OPEN',
   SET_SELECTED_PHOTO: 'SET_SELECTED_PHOTO',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA'
 };
 
 // Define initial state
@@ -13,6 +15,8 @@ const initialState = {
   isModalOpen: false,
   selectedPhoto: null,
   favPhotos: [],
+  photoData: [],
+  topicData: []
 };
 
 // Define the reducer function
@@ -26,6 +30,10 @@ function reducer(state, action) {
       return { ...state, isModalOpen: action.payload };
     case ACTIONS.SET_SELECTED_PHOTO:
       return { ...state, selectedPhoto: action.payload };
+    case 'SET_PHOTO_DATA':
+      return { ...state, photoData: action.payload };
+    case 'SET_TOPIC_DATA':
+      return { ...state, topicData: action.payload };
     default:
       throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
   }
@@ -34,6 +42,18 @@ function reducer(state, action) {
 function useApplicationData() {
   // Use useReducer with the reducer and initial state
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/api/photos")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: data }))
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/api/topics")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: data }))
+  }, []);
 
   // Action creators
   const updateToFavPhotoIds = (id) => {
